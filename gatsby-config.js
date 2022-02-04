8d0f75dd82ede8c2717fb036e80afab8f4a77f18
hy7721 <hykelly7721@gmail.com>
2022-02-04 18:52:14 +0900

    🌱 Add web fonts

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    title: "Gatsby + Netlify CMS Starter",
+    title: "HY KELLY Blog",
     description:
       "This repo contains an example business website that is built with Gatsby, and Netlify CMS.It follows the JAMstack architecture by using Git as a single source of truth, and Netlify for continuous deployment, and CDN distribution.",
   },
@@ -9,6 +9,10 @@ module.exports = {
     {
       resolve: "gatsby-plugin-sass",
       options: {
+        fonts: [
+          "Noto+Sans+KR\:100,300,400,500,700,900"
+        ],
+        display: "swap",
         sassOptions: {
           indentedSyntax: true,
         },
